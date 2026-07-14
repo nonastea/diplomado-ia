c51726de26858151d0b080a0caafda8e2e4b7f5f
nonastea <josu.medina451@gmail.com>
2026-07-13 21:58:47 -0400

    Add files via upload

---
+++ contenido.js
@@ -0,0 +1,24 @@
+// Generado automaticamente por Actualizar.command - no editar a mano
+window.CONTENIDO = {
+  "generado": "2026-07-14 01:46",
+  "curso": "Toma de decisiones basadas en datos con IA",
+  "archivos": {
+    "josue": {
+      "3":{"explicacion":"contenido/josue/unidad-3/Explicacion_Unidad3_Analisis_Descriptivo_Potenciado_por_IA.pdf","mapa":"contenido/josue/unidad-3/Unidad3_Mapa_Conceptual_y_Flashcards.html"},
+      "4":{"explicacion":"contenido/josue/unidad-4/Explicacion_Prototipado_de_proyectos_de_IA.pdf","mapa":"contenido/josue/unidad-4/Unidad4_Mapa_Conceptual_y_Flashcards.html"},
+      "5":{"explicacion":"contenido/josue/unidad-5/Explicacion_Unidad5_Analisis_Predictivo_y_Prescriptivo.pdf","mapa":"contenido/josue/unidad-5/Unidad5_Mapa_Conceptual_y_Flashcards.html"}
+    },
+    "jeanette": {
+      "3":{"explicacion_simple":"contenido/jeanette/unidad-3/Como_funciona_la_IA_explicada_para_ventas_unidad3_.html"},
+      "4":{"explicacion_simple":"contenido/jeanette/unidad-4/IA_explicada_para_ventas_Unidad4.html"},
+      "5":{"explicacion_simple":"contenido/jeanette/unidad-5/IA_explicada_para_ventas_Unidad5.html"},
+      "6":{"explicacion_simple":"contenido/jeanette/unidad-6/IA_explicada_para_ventas_Unidad6.html"}
+    }
+  },
+  "audiolibros": {
+    "3":"contenido/audiolibros/audiolibro_unidad3.html",
+    "4":"contenido/audiolibros/audiolibro_unidad4.html",
+    "5":"contenido/audiolibros/audiolibro_unidad5.html",
+    "6":"contenido/audiolibros/audiolibro_unidad6.html"
+  }
+};
